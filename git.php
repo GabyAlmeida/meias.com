d17aa3945e083fbc3662893b1fc8f90a655fef70
GabyAlmeida <gabysalmeida9182@gmail.com>
2018-09-10 19:44:22 -0300

    'mudancasnovas'

---
+++ git.php
@@ -0,0 +1,17 @@
+Git é um progama de versionamento de código
+github é um servidor na nuvem que armazana seus códigos 
+
+na prática
+
+criar novo repositorio no git
+
+git config --global user.name GabyAlmeida
+git config --global user.email gabysalmeida9182@gmail.com
+git init
+git clone https://github.com/GabyAlmeida/meias.com.git
+cole seu projeto na pasta
+no terminal
+entre na pasta
+git add *
+git commit -m 'nova pasta'
+git push
